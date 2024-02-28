b5cccaa893f592b2300ecbeca7a8e1056f1f7392
Jordy3D <fchetar@gmail.com>
2024-02-28 11:39:35 +1100

    Fix some style conflicts, fix broken split chapters

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ class EPUB {
         this.file = file;
         this.zip = new JSZip();
         this.toc = [];
+        this.tocContent = [];
         this.content = [];
         this.styles = [];
         this.images = [];
@@ -29,7 +30,11 @@ class EPUB {
         }
 
         this.toc = await this.getToc();
+        this.tocContent = await this.getTocContent();
         this.content = await this.getContent();
+
+        this.mergedContent = this.mergeContent();
+
         this.styles = await this.getStyles();
         this.images = await this.getImages();
     }
@@ -54,7 +59,7 @@ class EPUB {
         return toc;
     }
 
-    async getContent() {
+    async getTocContent() {
         let content = [];
         for (let i = 0; i < this.toc.length; i++) {
             let item = this.toc[i];
@@ -65,10 +70,91 @@ class EPUB {
             else
                 html = await this.zip.file(this.rootFolder + "/" + item.content).async("string");
 
-            content.push(html);
+            // save the file name as the key and the content as the value
+            let name = item.content.split("/").pop();
+            name = name.split(".")[0];
+            content.push({ name: name, content: html });
         }
         return content;
     }
+
+    async getContent() {
+        // load content from content.opf's manifest
+        // only the html/xhtml files are needed
+        let content = [];
+        let contentFile = null;
+        if (this.rootFolder === "")
+            contentFile = await this.zip.file("content.opf").async("string");
+        else
+            contentFile = await this.zip.file(this.rootFolder + "/content.opf").async("string");
+
+        let parser = new DOMParser();
+        let xmlDoc = parser.parseFromString(contentFile, "text/xml");
+        let manifest = xmlDoc.getElementsByTagName("manifest")[0];
+        let items = manifest.getElementsByTagName("item");
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+
+            // only get the html/xhtml files
+            if (!item.getAttribute("href").endsWith(".html") && !item.getAttribute("href").endsWith(".xhtml"))
+                continue;
+
+            let href = item.getAttribute("href");
+            let html = null;
+            if (this.rootFolder === "")
+                html = await this.zip.file(href).async("string");
+            else
+                html = await this.zip.file(this.rootFolder + "/" + href).async("string");
+
+            // save the file name as the key and the content as the value
+            let name = href.split("/").pop();
+            name = name.split(".")[0];
+            content.push({ name: name, content: html });
+        }
+        return content;
+    }
+
+
+    mergeContent() {
+        let mergedContent = [];
+
+        // make a copy of each array
+        let tempTocContent = this.tocContent.slice();
+        let tempContent = this.content.slice();
+       
+        // go through the content array and add in the content from the content array between the tocContent where the .name matches
+        // ie, tocContent has chapter1, chapter2, chapter3, and content has chapter1, insert1, chapter1_1, chapter2, insert2, chapter2_1, chapter3
+        // the mergedContent array should be chapter1, insert1, chapter1_1, chapter2, insert2, chapter2_1, chapter3
+        let index = 0;
+        for (let i = 0; i < tempContent.length; i++) {
+            let name = tempContent[i].name;
+            // if the name exists in the tocContent array, add it to the mergedContent array in the last position
+            // if the name doesn't exist in the tocContent array, add it to the mergedContent array in the current position
+            let found = false;
+            for (let j = 0; j < tempTocContent.length; j++) {
+                if (tempTocContent[j].name === name) {
+                    mergedContent.push(tempTocContent[j]);
+                    tempTocContent.splice(j, 1);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                mergedContent.push(tempContent[i]);
+            }
+        }
+
+
+        // add the remaining content from the tocContent array to the end of the mergedContent array
+        for (let i = 0; i < tempTocContent.length; i++) {
+            mergedContent.push(tempTocContent[i]);
+        }
+
+        // TODO: merge the mergedContent array such that split chapters are merged together
+
+        return mergedContent;
+    }
+
 
     async getStyles() {
         // load the css files and store them in a dictionary with the file name as the key
@@ -121,7 +207,7 @@ function displayToc() {
         link.textContent = item.text;
         link.href = "";
         link.onclick = function () {
-            content.innerHTML = epub.content[i];
+            content.innerHTML = epub.tocContent[i];
         };
         toc.appendChild(link);
     }
@@ -155,7 +241,7 @@ function replaceContent(content) {
 }
 
 function loadChapter(index) {
-    let content = replaceContent(epub.content[index]);
+    let content = replaceContent(epub.mergedContent[index].content);
     document.getElementById("epub-content").innerHTML = content;
 
     markActiveLink();
@@ -213,7 +299,13 @@ document.getElementById("epub-toc").addEventListener("click", function (event) {
     event.preventDefault();
 
     if (event.target.tagName === "A") {
-        let index = Array.from(event.target.parentNode.children).indexOf(event.target);
+
+        // find the element of mergedContent that matches the clicked link
+        let tocIndex = Array.from(event.target.parentNode.children).indexOf(event.target);
+
+        // find the index of the clicked link in the mergedContent array by the name
+        let index = epub.mergedContent.findIndex(x => x.name === epub.toc[tocIndex].content.split("/").pop().split(".")[0]);
+
         loadChapter(index);
         chapter = index;
     }
@@ -221,7 +313,7 @@ document.getElementById("epub-toc").addEventListener("click", function (event) {
 
 // when the user clicks on the next chapter button, display the next chapter
 document.getElementById("next").addEventListener("click", function (event) {
-    if (chapter < epub.content.length - 1) {
+    if (chapter < epub.tocContent.length - 1) {
         chapter++;
         loadChapter(chapter);
     }
@@ -245,13 +337,9 @@ document.addEventListener("wheel", function (event) {
     else
         scrollDirection = -1;
 
-    console.log("scroll direction: " + scrollDirection);
-
     // if there is no scroll bar 
     if (document.documentElement.scrollHeight === document.documentElement.clientHeight)
     {
-        console.log("no scroll bar");
-
         // if the user is scrolling up
         if (scrollDirection === 1 && chapter > 0)
         {
@@ -259,7 +347,7 @@ document.addEventListener("wheel", function (event) {
             loadChapter(chapter);
         }
         // if the user is scrolling down
-        else if (scrollDirection === -1 && chapter < epub.content.length - 1)
+        else if (scrollDirection === -1 && chapter < epub.mergedContent.length - 1)
         {
             chapter++;
             loadChapter(chapter);
@@ -267,15 +355,12 @@ document.addEventListener("wheel", function (event) {
     }
     else
     {
-        console.log("scroll bar");
-
         let scrollHeight = document.documentElement.scrollHeight;
         let clientHeight = document.documentElement.clientHeight;
         let scrollY = window.scrollY;
 
         let bottom = (scrollHeight - clientHeight - scrollY);
         let atBottom = bottom <= 1;
-        console.log(`bottom: ${bottom}, atBottom: ${atBottom}`);
 
         // if the user is scrolling up
         if (scrollDirection === 1 && window.scrollY === 0 && chapter > 0)
@@ -287,7 +372,7 @@ document.addEventListener("wheel", function (event) {
             window.scrollTo(0, document.documentElement.scrollHeight);
         }
         // if the user is scrolling down
-        else if (scrollDirection === -1 && atBottom && chapter < epub.content.length - 1)
+        else if (scrollDirection === -1 && atBottom && chapter < epub.mergedContent.length - 1)
         {
             chapter++;
             loadChapter(chapter);
